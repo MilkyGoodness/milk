c7bb49892f66f7b9d50c4d1457982ea22170aa93
Sarah Hum <sarshum@gmail.com>
2012-11-11 09:38:23 -0500

    Splash page relink css

--- splash/js/scripts.js
+++ splash/js/scripts.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
         $('body').scrollTop(0);
     });
 
+    if (Math.floor(Math.random()*11) > 5) {
+        $("iframe").attr("src", "https://docs.google.com/spreadsheet/embeddedform?formkey=dFE2LXpxZjNZOXJqNHppXzJha3poMGc6MQ&bc=77C4FF&tc=fff&f=helvetica&ttl=0");
+        console.log("inside");
+    }
 
+    console.log("what");
 
 });
 
