1df8d613e6bfc7c8ca9c1d62f8cf6193289a1427
Sarah Hum <sarshum@gmail.com>
2012-11-10 20:00:11 -0500

    top menu lock and notification changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    $(window).unload(function() {
+        $('body').scrollTop(0);
+    });
+
     $("#confirm").click(function() {
         $("a.confirm").removeClass('confirm').addClass('event');
     });
@@ -21,16 +25,38 @@ $(document).ready(function(){
             $("#featured-list").fadeOut();
             $("#popular-list").fadeOut();
             $("#item-list").fadeIn();
+            $("#categories, #sort-by").fadeIn();
         } else {
             $("#item-list").fadeOut();
             $("#featured-list").fadeIn();
             $("#popular-list").fadeIn();
+            $("#categories, #sort-by").fadeOut();
         }
     });
     
     $(".items").click(function() {
         $("#notification").fadeIn().delay(1200).fadeOut();
-        $("#milk-kart").css("background-position", -40);
+        $("#milk-kart").css("background-position", function() {
+            return ($("#milk-kart").css("background-position").split('px')[0] - 40);
+        });
+    });
+
+    $(document).scroll(function() {
+        if ($(document).scrollTop() >= 230) {
+            $('form').css('position', 'fixed');
+            $('form').css('top', 20);
+        } else {
+            $('form').css('position', 'absolute');
+            $('form').css('top', 250);
+        }
+        if ($(document).scrollTop() >= 310) {
+            $('#blue').css('position', 'fixed');
+            $('#blue').css('top', -310)
+        } else {
+            $('#blue').css('position', 'absolute');
+            $('#blue').css('top', 0)
+
+        }
     });
 
 });
