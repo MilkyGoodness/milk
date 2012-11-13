1c5c9a49e72b9c8c65539d22b8dd2ae25fbf4c57
heyqule <heyqule@HQSNV3-Ubuntu64>
2012-11-12 23:46:59 -0500

    Amazon Commit Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 
     $(window).unload(function() {
         $('body').scrollTop(0);
-    });
+    });   
     
     $(document).scroll(function() {
         if ($(document).scrollTop() >= 250) {
